678bf42769a3e5d6471797e4912203bb028a4be0
SAKSHI JADHAV <sakshijadhav8856@gmail.com>
2025-09-12 11:15:22 +0530

    Add files via upload

---
+++ revision/main46.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a;
+    int b;
+
+    scanf("%d" , &a);
+    scanf("%d" , &b);
+
+    printf("The sum is %d \n" , a + b);
+    printf("The difference is %d \n" , a - b);
+    printf("The multiplication is %d \n" , a*b);
+    printf("The division is %d \n" , a / b);
+
+    
+
+    return 0;
+}
